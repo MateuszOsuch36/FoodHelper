977c7004043276b813b704919d0a580d94991867
Mathew <kuskowski09@gmail.com>
2021-06-01 09:24:02 +0200

    Changed Comments in validation.php

--- Validation.php
+++ Validation.php
@@ -1,17 +1,20 @@
 <?php
+
+//------
+// Mateusz Kuskowski
+//------
+
     // Załączamy klasy kont
     require_once('Accounts.php');
     session_start();
 
-    // Gdy zdecydujemy się stworzyć nowego użytkownika, będzie on dostępny 
-    // do momentu wylogowania. Jest na ten moment zdefiniowane 3 użytkowników ( plik Accounts.php )
 
     if(!isset($_SESSION['isLogged']))
     {
         if ($_POST['userAction'] == 'logIn') 
         {
             // Gdy użytkownik wypełnił formę do logowania
-            
+
 
         }
         else if ( $_POST['userAction'] == 'register' )
